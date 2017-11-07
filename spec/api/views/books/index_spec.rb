8771a3e1d633430451e6c5ff4787ea609edb45d8
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-11-07 12:35:29 +0200

    Added Books Index page

---
+++ spec/api/views/books/index_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe Api::Views::Books::Index, type: :view do
+  # let(:exposures) { Hash[foo: 'bar'] }
+  # let(:template)  { Hanami::View::Template.new('apps/api/templates/books/index.html.slim') }
+  # let(:view)      { described_class.new(template, exposures) }
+  # let(:rendered)  { view.render }
+  #
+  # it 'exposes #foo' do
+  #   pending 'This is an auto-generated test. Edit it and add your own tests.'
+  #
+  #   # Example
+  #   expect(view.foo).to eq exposures.fetch(:foo)
+  # end
+end
