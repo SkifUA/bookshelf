b5a888c21bfea24804768fc29c443bd237b87d37
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-11-07 19:48:56 +0200

    Added dynamic data for books#index

--- apps/api/controllers/books/index.rb
+++ apps/api/controllers/books/index.rb
@@ -2,7 +2,10 @@ module Api::Controllers::Books
   class Index
     include Api::Action
 
+    expose :books
+
     def call(params)
+      @books = BookRepository.new.all
     end
   end
 end
--- spec/api/views/books/index_spec.rb
+++ spec/api/views/books/index_spec.rb
@@ -1,13 +1,32 @@
 RSpec.describe Api::Views::Books::Index, type: :view do
-  # let(:exposures) { Hash[foo: 'bar'] }
-  # let(:template)  { Hanami::View::Template.new('apps/api/templates/books/index.html.slim') }
-  # let(:view)      { described_class.new(template, exposures) }
-  # let(:rendered)  { view.render }
-  #
-  # it 'exposes #foo' do
-  #   pending 'This is an auto-generated test. Edit it and add your own tests.'
-  #
-  #   # Example
-  #   expect(view.foo).to eq exposures.fetch(:foo)
-  # end
+  let(:exposures) { Hash[books: []] }
+  let(:template)  { Hanami::View::Template.new('apps/api/templates/books/index.html.slim') }
+  let(:view)      { described_class.new(template, exposures) }
+  let(:rendered)  { view.render }
+
+  it 'exposes #books' do
+    expect(view.books).to eq exposures.fetch(:books)
+  end
+
+  describe 'when there are no books' do
+    it 'shows a placeholder message' do
+      expect(rendered).to include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
+
+  describe 'when there are books' do
+    let(:book1)     { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2)     { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it 'lists them all' do
+      expect(rendered.scan(/class="book"/).count).to eq 2
+      expect(rendered).to include('Refactoring')
+      expect(rendered).to include('Domain Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      expect(rendered).not_to include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
 end
